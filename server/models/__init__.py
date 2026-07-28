c803947e14a0b70ef60a8cca05530ac85996be53
SamuelMutiso <muuosamuel335@gmail.com>
2026-07-28 21:26:48 +0300

    Aggregate model imports

---
+++ server/models/__init__.py
@@ -0,0 +1,9 @@
+from .user import User
+from .vehicle import Vehicle
+from .feature import Feature
+from .vehicle_feature import vehicle_features
+from .driver import Driver
+from .driver_application import DriverApplication
+from .booking import Booking
+from .contact_message import ContactMessage
+__all__ = ['User', 'Vehicle', 'Feature', 'vehicle_features', 'Driver', 'DriverApplication', 'Booking', 'ContactMessage']
